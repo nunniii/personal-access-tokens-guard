ae165735420021de77dbb31652e578f26bfbed20
Mateus Nunes <mateusnss@protonmail.ch>
2022-01-24 14:02:09 -0300

    redistributed functions

--- src/auth.go
+++ src/auth.go
@@ -1,31 +1,5 @@
 package main
 
-import(
-	"fmt"
-)
-
-
-func authRequest() bool {
-
-		var receivedKey string;
-		var authorized bool;
-
-		fmt.Print("Enter the password: ")
-		fmt.Print("\033[8m") // Hide input
-		fmt.Scan(&receivedKey)
-		fmt.Print("\033[28m") // Show input
-		// fmt.Print(receivedKey)
-
-	
-	var hash string = "912ec803b2ce49e4a541068d495ab570"; 
-
-	if (createHash(receivedKey) == hash){
-		fmt.Print("Authorized")
-		authorized = true;
-	}else{
-		fmt.Print("Not authorized")
-		authorized = false;
-	}
-	return authorized;
+func authRequest(receivedKey string, hash string) bool {
+	return createHash(receivedKey) == hash;
 }
-
--- src/main.go
+++ src/main.go
@@ -8,7 +8,25 @@ import(
 func main(){
 
 	arg := os.Args[1]
-	authRequest();
+	var receivedKey string;
+	var hash string = "e10adc3949ba59abbe56e057f20f883e"; // hash de 123456
+	var authorized bool;
+
+
+	fmt.Print("Enter the password: ")
+	fmt.Print("\033[8m") // Hide input
+	fmt.Scan(&receivedKey)
+	fmt.Print("\033[28m") // Show input
+
+
+
+	 authorized = authRequest(receivedKey, hash);
+	if(authorized){
+		fmt.Print("Authorized")
+	}else{
+		fmt.Print("Not authorized")
+	}
+	
 
 
 	
